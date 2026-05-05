697ee89ee03c3db3b7be6439f79826799c804601
Ayesha Azhar <ayeshaazher04@gmail.com>
2026-05-05 19:23:35 +0500

    Update firebase_config.dart

--- lib/backend/firebase/firebase_config.dart
+++ lib/backend/firebase/firebase_config.dart
@@ -7,12 +7,12 @@ Future initFirebase() async {
   if (kIsWeb) {
     await Firebase.initializeApp(
         options: FirebaseOptions(
-            apiKey: "AIzaSyDxgJcmhd3nD_so5A7fyBVWMDxe-DMYhyM",
-            authDomain: "ayeshwebsite-f0494.firebaseapp.com",
-            projectId: "ayeshwebsite-f0494",
-            storageBucket: "ayeshwebsite-f0494.firebasestorage.app",
-            messagingSenderId: "882112364789",
-            appId: "1:882112364789:web:9ea97dd0e45eb38926c5bc"));
+            apiKey: "",
+            authDomain: "",
+            projectId: "",
+            storageBucket: "",
+            messagingSenderId: "",
+            appId: ""));
   } else {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
